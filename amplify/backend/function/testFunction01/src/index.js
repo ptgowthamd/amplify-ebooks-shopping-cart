69446ad9bdf54740bb0a0de67087e07a223aff1b
Gowtham Dasari <gowthamdpt@gmail.com>
2025-08-17 19:08:42 +0530

    fix amplify.yml

--- amplify/backend/function/testFunction01/src/index.js
+++ amplify/backend/function/testFunction01/src/index.js
@@ -5,6 +5,7 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    console.log(`test logging from testFunction01`);
     return {
         statusCode: 200,
         body: JSON.stringify('Hello from AWS Lambda!, test change3 new, may be this time this change is not ignored'),
