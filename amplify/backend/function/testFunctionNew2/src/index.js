d917e6e9f9ca99a16c3d5a5536fe90d4c028c639
Gowtham Dasari <gowthamdpt@gmail.com>
2025-08-18 21:15:30 +0530

    Test change for Lambda function

--- amplify/backend/function/testFunctionNew2/src/index.js
+++ amplify/backend/function/testFunctionNew2/src/index.js
@@ -12,6 +12,6 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify('Hello from Lambda!, test change'),
+        body: JSON.stringify('Hello from Lambda!, test change, again'),
     };
 };
